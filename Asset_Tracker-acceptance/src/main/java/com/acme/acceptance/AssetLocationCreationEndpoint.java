e0d8b9a898233ac0903f85c59fa1da13b7a0ef45
Michael Cawte <mcawte@gmail.com>
2022-02-28 21:43:20 +1300

    Update quarkus, increase version number and commit previous chnages to AssetLocationCreationEndpoint

--- Asset_Tracker-acceptance/src/main/java/com/acme/acceptance/AssetLocationCreationEndpoint.java
+++ Asset_Tracker-acceptance/src/main/java/com/acme/acceptance/AssetLocationCreationEndpoint.java
@@ -1,12 +1,14 @@
 package com.acme.acceptance;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.acme.Config;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.text.MatchesPattern;
@@ -21,29 +23,44 @@ public class AssetLocationCreationEndpoint {
         this.config = config;
     }
 
-    @Given("Given I have some valid asset location JSON")
+    @Given("I have some valid asset location JSON")
     public void iHaveSomeValidAssetLocationJson() {
         validJson = "{\"timestampUtc\": 10, \"lng\": 30, \"lat\": 40}";
         request = given()
-            .baseUri(config.get("acceptance.assets-endpoint"));
-        request.accept("application/json");
-        request.body(validJson);
+            .baseUri(config.get("acceptance.assets-endpoint"))
+            .contentType(ContentType.JSON)
+        .body(validJson);
     }
 
-    @When("When I post the JSON to the asset location creation endpoint")
-    public void iPostTheJsonToTheAssetLocationCreationEndpoint() {
+    // @Given("I have some valid asset location JSON")
+    // public void i_have_some_valid_asset_location_json() {
+    //     // Write code here that turns the phrase above into concrete actions
+    //     validJson = "{\"timestampUtc\": 10, \"lng\": 30, \"lat\": 40}";
+    //     request = given()
+    //         .baseUri(config.get("acceptance.assets-endpoint"));
+    //     request.accept("application/json");
+    //     request.body(validJson);
+    //     //throw new io.cucumber.java.PendingException();
+    // }
+
+    @When("I post the JSON to the asset location creation endpoint")
+    public void i_post_the_json_to_the_asset_location_creation_endpoint() {
         response = request.post();
     }
 
-    @Then("I should receive a {int} response")
-    public void iShouldReceive200Response(Integer statusCode) {
+    @Then("I should receive a {int} status code response")
+    public void iShouldReceiveStatusCodeResponse(Integer statusCode) {
         response.then().statusCode(statusCode);
     }
 
-    @And("And the returned JSON should include a generated asset location id")
-    public void iShouldReceiveResponse() {
-        response.then().assertThat()
-        .body("id", MatchesPattern
+    @And("the returned JSON should include a generated asset location id")
+    public void the_returned_json_should_include_a_generated_asset_location_id() {
+        String id = response.jsonPath().getString("id");
+        System.out.println("The id is: " + id);
+        assertThat(id, MatchesPattern
         .matchesPattern("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}"));
+
+
+        
     }
 }
--- Asset_Tracker-acceptance/src/main/resources/com/acme/asset_location_creation_endpoint.feature
+++ Asset_Tracker-acceptance/src/main/resources/com/acme/asset_location_creation_endpoint.feature
@@ -1,8 +1,8 @@
 Feature: Asset Location Creation Endpoint
     As an asset owner, I want an asset location creation endpoint, so that I can track the location of my endpoints.
 
-    Scenario:
+    Scenario: A new asset gets added fine
         Given I have some valid asset location JSON
         When I post the JSON to the asset location creation endpoint
-        Then I should receive a 200 response
+        Then I should receive a 200 status code response
         And the returned JSON should include a generated asset location id
